ba22007975f25951fd45b67ca2b124e01ebed000
fzxcp3 <835749207@qq.com>
2016-06-26 23:55:08 +0800

    web

--- web/mysite/views.py
+++ web/mysite/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import time
 # Create your views here.
+
+def index(request):
+    time.sleep(3)
+    return HttpResponse("Test")
